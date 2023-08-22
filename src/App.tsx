c7083c7efbef51c9c9059072ceb5b156041bc1a2
Rodrigo Guardia <rodrigo.guardia316@gmail.com>
2023-08-22 19:59:31 -0400

    algo x

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>
-          el que lea esto es sus amogus x2
+          dios mediante
         </p>
         <a
           className="App-link"
@@ -23,6 +23,44 @@ function App() {
         </a>
       </header>
     </div>
+       <div className="App">
+       <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+         <p>
+           Edit <code>src/App.tsx</code> and save to reload.
+         </p>
+         <p>
+           dios mediante 2
+         </p>
+         <a
+           className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer"
+         >
+           Learn React
+         </a>
+       </header>
+     </div>
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <p>
+            dios mediante
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
