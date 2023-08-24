5f69465b26eedeeeae78d4d91295e2816c75ac76
Ogreaggressive <nicolascari.rz@gmail.com>
2023-08-24 01:10:21 -0400

    changing stuff

--- src/functions.js
+++ src/functions.js
@@ -2,21 +2,4 @@
 function sumar(a, b) {
     return a + b;
   }
-
-function sumar2(a, b) {
-    return a + b;
-  }
-
-  function sumar3(a, b) {
-    return a + b;
-  }
-  
-  function sumar4(a, b) {
-    return a + b;
-  }
-
-  function multiplicar(a, b) {
-    return a * b;
-  }
-
-  export {sumar, multiplicar};
+  export {sumar};
